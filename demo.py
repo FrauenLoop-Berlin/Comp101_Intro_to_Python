c64052cdd4087d9803fb547ff20f6b34a25b4625
Nathalie Scherf <nascherf@yahoo.se>
2023-01-23 21:14:43 +0100

    Week 2: variables and data types

---
+++ demo.py
@@ -0,0 +1,48 @@
+# run using the button in the right hand side corner
+# use the terminal `python3 demo.py`
+
+# define a variable, name
+msg = 'Hello'
+# print the content () on the terminal
+print(msg)
+my_name = 'Nathalie'
+my_name = 'Anna'
+# Print the value of a variable
+print(my_name)
+# string
+print('Ivonita')
+# Number: Integer 
+my_age = 36
+# or Float
+temp = 2.2
+# <class 'int'>
+print(type(my_age))
+
+## Calculate the year I was born:
+my_birthyear = 2023 - 36
+
+current_year = 2023
+my_birthyear = current_year - my_age
+print(my_birthyear)
+
+## Data type string, integer, float, 
+
+# boolean, 
+mentor = True 
+mentor = False 
+
+# list, 
+my_hobbies = [ 'running', 'coding', 'eating', 123, False, [1,2,3] ]
+
+# dictionary (dict): key: value
+my_pet = {
+  'name': 'Susi',
+  'animal': 'cat',
+  'age': 2,
+  'cute': True,
+}
+
+my_tweet = {
+  'text': 'Hello',
+  'date': '2023.01.23'
+}
